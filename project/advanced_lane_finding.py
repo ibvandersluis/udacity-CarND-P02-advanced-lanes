3c77a9d72feaa6bd8617963f585f2e87cee8eb78
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2021-04-12 09:50:19 +0100

    Undistort camera images

--- project/advanced_lane_finding.py
+++ project/advanced_lane_finding.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-from p2_functions import calibrate_camera
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from p2_functions import calibrate_camera, pipeline
 
 if __name__ == "__main__":
+  # Get camera calibration info
   ret, mtx, dist, rvecs, tvecs = calibrate_camera('camera_cal/', 9, 6)
+  
+  img = mpimg.imread('test_images/test5.jpg')
+  
+  result = pipeline(img, mtx, dist, rvecs, tvecs)
 
+  f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+  f.tight_layout()
+  ax1.imshow(img)
+  ax1.set_title('Before Pipeline', fontsize=50)
+  ax2.imshow(result)
+  ax2.set_title('After Pipeline', fontsize=50)
+  plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+  plt.show()
--- project/p2_functions.py
+++ project/p2_functions.py
@@ -377,3 +377,8 @@ def measure_curvature_real():
     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 
     return left_curverad, right_curverad
+
+def pipeline(img, mtx, dist, rvecs, tvecs):
+    result = cv2.undistort(img, mtx, dist, None, mtx)
+
+    return result
