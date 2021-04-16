a8dda2b0f1d2e10362e16bd6bba594eec51539d8
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2021-04-16 14:37:19 +0100

    Improve functions

--- project/p2_functions.py
+++ project/p2_functions.py
@@ -4,6 +4,29 @@ import matplotlib.image as mpimg
 import numpy as np
 import os
 
+class Lane:
+    def __init__(self):
+        # Was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        # Average x values of the fitted line over the last n iterations
+        self.best_x = None
+        # Polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        # Polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]
+        # Radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        # Distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        # Difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        # x values for detected line pixels
+        self.px_x = None
+        # y values for detected line pixels
+        self.px_y = None
+
 def calibrate_camera(dirpath, nx, ny):
     """
     Performs camera calibration using images found in a supplied directory.
@@ -97,34 +120,27 @@ def hls_select(image, thresh=(0, 255)):
 
     return binary_output
 
-def hist(image):
-    return np.sum(image[image.shape[0]//2:,:], axis=0)
-
-def find_lane_pixels(binary_warped):
-    # Take a histogram of the bottom half of the image
+def find_lane_pixels(binary_warped, nwindows=9, margin=100, minpix=50):
+    # Take a histogram of the bottom half of the image to find lane positions
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+
     # Create an output image to draw on and visualize the result
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0]//2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    leftx_base = np.argmax(histogram[:midpoint]) # Find highest point on left
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint # Do same on right
 
-    # HYPERPARAMETERS
-    # Choose the number of sliding windows
-    nwindows = 9
-    # Set the width of the windows +/- margin
-    margin = 100
-    # Set minimum number of pixels found to recenter window
-    minpix = 50
-
-    # Set height of windows - based on nwindows above and image shape
+    # Set height of windows - based on nwindows argument and image shape
     window_height = np.int(binary_warped.shape[0]//nwindows)
+
     # Identify the x and y positions of all nonzero pixels in the image
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
+
     # Current positions to be updated later for each window in nwindows
     leftx_current = leftx_base
     rightx_current = rightx_base
@@ -144,10 +160,10 @@ def find_lane_pixels(binary_warped):
         win_xright_high = rightx_current + margin
 
         # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
-        (win_xleft_high,win_y_high),(0,255,0), 2)
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),
-        (win_xright_high,win_y_high),(0,255,0), 2)
+        # cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+        # (win_xleft_high,win_y_high),(0,255,0), 2)
+        # cv2.rectangle(out_img,(win_xright_low,win_y_low),
+        # (win_xright_high,win_y_high),(0,255,0), 2)
 
         # Identify the nonzero pixels in x and y within the window #
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
@@ -181,7 +197,6 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-
 def fit_polynomial(binary_warped):
     # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
@@ -202,43 +217,44 @@ def fit_polynomial(binary_warped):
         right_fitx = 1*ploty**2 + 1*ploty
 
     ## Visualization ##
+    # Colour in the region between the lanes
+    left_boundary = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    right_boundary = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    lane = np.hstack((left_boundary, right_boundary))
+
+    cv2.fillPoly(out_img, np.int_([lane]), (0,255, 0))
+
     # Colors in the left and right lane regions
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
 
     # Plots the left and right polynomials on the lane lines
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
+    # plt.plot(left_fitx, ploty, color='yellow')
+    # plt.plot(right_fitx, ploty, color='yellow')
 
-    return out_img
+    return out_img, left_fit, right_fit, ploty
 
 def fit_poly(img_shape, leftx, lefty, rightx, righty):
-    ### TO-DO: Fit a second order polynomial to each with np.polyfit() ###
+    # Fit a second-order polynomial to left_ and right_fit
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
+
     # Generate x and y values for plotting
     ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
-    ### TO-DO: Calc both polynomials using ploty, left_fit and right_fit ###
+
+    # Calculate both polynomials using ploty, left_fit and right_fit
     left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
     return left_fitx, right_fitx, ploty
 
-def search_around_poly(binary_warped, left_fit, right_fit):
-    # HYPERPARAMETER
-    # Choose the width of the margin around the previous polynomial to search
-    # The quiz grader expects 100 here, but feel free to tune on your own!
-    margin = 100
-
+def search_around_poly(binary_warped, left_fit, right_fit, margin=100):
     # Grab activated pixels
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
 
-    ### TO-DO: Set the area of search based on activated x-values ###
-    ### within the +/- margin of our polynomial function ###
-    ### Hint: consider the window areas for the similarly named variables ###
-    ### in the previous quiz, but change the windows to our new search area ###
+    # Set the area of search based on activated x-values
     left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy +
                     left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) +
                     left_fit[1]*nonzeroy + left_fit[2] + margin)))
@@ -259,6 +275,7 @@ def search_around_poly(binary_warped, left_fit, right_fit):
     # Create an image to draw on and an image to show the selection window
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
     window_img = np.zeros_like(out_img)
+
     # Color in left and right line pixels
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
@@ -286,102 +303,20 @@ def search_around_poly(binary_warped, left_fit, right_fit):
 
     return result
 
-def generate_data():
-    '''
-    Generates fake data to use for calculating lane curvature.
-    In your own project, you'll ignore this function and instead
-    feed in the output of your lane detection algorithm to
-    the lane curvature calculation.
-    '''
-    # Set random seed number so results are consistent for grader
-    # Comment this out if you'd like to see results on different random data!
-    np.random.seed(0)
-    # Generate some fake data to represent lane-line pixels
-    ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
-    quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
-    # For each y position generate random x position within +/-50 pix
-    # of the line base position in each case (x=200 for left, and x=900 for right)
-    leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51)
-                                    for y in ploty])
-    rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51)
-                                    for y in ploty])
-
-    leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
-    rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
-
-
-    # Fit a second order polynomial to pixel positions in each fake lane line
-    left_fit = np.polyfit(ploty, leftx, 2)
-    right_fit = np.polyfit(ploty, rightx, 2)
-
-    return ploty, left_fit, right_fit
-
-def generate_data_metres(ym_per_pix, xm_per_pix):
-    '''
-    Generates fake data to use for calculating lane curvature.
-    In your own project, you'll ignore this function and instead
-    feed in the output of your lane detection algorithm to
-    the lane curvature calculation.
-    '''
-    # Set random seed number so results are consistent for grader
-    # Comment this out if you'd like to see results on different random data!
-    np.random.seed(0)
-    # Generate some fake data to represent lane-line pixels
-    ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
-    quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
-    # For each y position generate random x position within +/-50 pix
-    # of the line base position in each case (x=200 for left, and x=900 for right)
-    leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
-                                    for y in ploty])
-    rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
-                                    for y in ploty])
-
-    leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
-    rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
-
-    # Fit a second order polynomial to pixel positions in each fake lane line
-    # Fit new polynomials to x,y in world space
-    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
-    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
-    
-    return ploty, left_fit_cr, right_fit_cr
-
-def measure_curvature_pixels():
-    '''
-    Calculates the curvature of polynomial functions in pixels.
-    '''
-    # Start by generating our fake example data
-    # Make sure to feed in your real data instead in your project!
-    ploty, left_fit, right_fit = generate_data()
-
-    # Define y-value where we want radius of curvature
-    # We'll choose the maximum y-value, corresponding to the bottom of the image
-    y_eval = np.max(ploty)
-
-    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
-    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
-
-    return left_curverad, right_curverad
-
-def measure_curvature_real():
+def measure_curvature(left_fit, right_fit, ploty):
     '''
     Calculates the curvature of polynomial functions in meters.
     '''
     # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30/720 # meters per pixel in y dimension
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
-
-    # Start by generating our fake example data
-    # Make sure to feed in your real data instead in your project!
-    ploty, left_fit_cr, right_fit_cr = generate_data_metres(ym_per_pix, xm_per_pix)
+    ym_per_pix = 3/80.0 # 3m long dashes take up about 80 px
+    xm_per_pix = 3.7/598 # 3.7m lane width is about 598 px
 
     # Define y-value where we want radius of curvature
-    # We'll choose the maximum y-value, corresponding to the bottom of the image
     y_eval = np.max(ploty)
 
     # Calculation of R_curve (radius of curvature)
-    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    left_curverad = ((1 + (2*left_fit[0]*y_eval*ym_per_pix + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    right_curverad = ((1 + (2*right_fit[0]*y_eval*ym_per_pix + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
 
     return left_curverad, right_curverad
 
@@ -417,8 +352,8 @@ def pipeline(img, mtx, dist, rvecs, tvecs):
     # Set the source points for the transformation
     src = np.float32([[190, 720],
                       [593, 450],
-                      [686, 450],
-                      [1115, 720]])
+                      [687, 450],
+                      [1090, 720]])
 
     # Set the destination points for the transformation
     dst = np.float32([[330, 720],
@@ -433,6 +368,19 @@ def pipeline(img, mtx, dist, rvecs, tvecs):
     tf_M_inv = cv2.getPerspectiveTransform(dst, src)
 
     # Tranform the perspective of the image to top-down
-    tf_img = cv2.warpPerspective(binary, tf_M, img_size)
+    tf_binary = cv2.warpPerspective(binary, tf_M, img_size)
 
-    return tf_img
+    # Find the lane pixels and fit a polynomial to define right/left boundaries
+    out_img, left_fit, right_fit, ploty = fit_polynomial(tf_binary)
+
+    left_radius, right_radius = measure_curvature(left_fit, right_fit, ploty)
+    print(left_radius, right_radius)
+
+    # Return the detected lane visuals to the original perspective
+    unwarp = cv2.warpPerspective(out_img, tf_M_inv, img_size)
+
+    # Overlay result on top of original image
+    result = cv2.addWeighted(img, 1, unwarp, 0.3, 0)
+    # cv2.imwrite('./output_images/step4.jpg', cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
+
+    return result
