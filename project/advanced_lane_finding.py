f4bcf2ccf18a22c24082e34cada44aee4c7f719a
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2021-04-16 17:18:12 +0100

    First attempt at processing video

--- project/advanced_lane_finding.py
+++ project/advanced_lane_finding.py
@@ -1,40 +1,14 @@
 #!/usr/bin/env python3
 
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import os
-from p2_functions import calibrate_camera, pipeline
+from moviepy.editor import VideoFileClip
+from p2_functions import Lane, pipeline
 
+if __name__ == '__main__':
+    left_lane = Lane()
+    right_lane = Lane()
 
-if __name__ == "__main__":
-  # Get camera calibration info
-  ret, mtx, dist, rvecs, tvecs = calibrate_camera('camera_cal/', 9, 6)
-  
-  test_images = os.listdir('test_images/') # Get file names for images
+    output = 'output_video.mp4'
+    clip = VideoFileClip('project_video.mp4')
+    output_clip = clip.fl_image(lambda image: pipeline(image, left_lane, right_lane))
 
-  for photo in test_images:
-      # Get raw image (still distorted)
-      # img_raw = mpimg.imread('test_images/test6.jpg')
-      img_raw = mpimg.imread('test_images/' + photo)
-
-      # Undistort the image
-      img = cv2.undistort(img_raw, mtx, dist, None, mtx)
-      
-      # Run undistorted image through pipeline
-      result = pipeline(img, mtx, dist, rvecs, tvecs)
-
-      savefile = os.path.splitext(photo)[0]
-      savefile += '_out.jpg'
-
-      cv2.imwrite('./output_images/' + savefile, cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
-
-  # Compare side by side
-  f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-  f.tight_layout()
-  ax1.imshow(img)
-  ax1.set_title('Before Pipeline', fontsize=50)
-  ax2.imshow(result, cmap='gray')
-  ax2.set_title('After Pipeline', fontsize=50)
-  plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-  plt.show()
+    output_clip.write_videofile(output, audio=False)
