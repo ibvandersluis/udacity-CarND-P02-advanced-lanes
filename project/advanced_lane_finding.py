fa81c2e467a0a56cfad238af19bd0702a743f73e
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2021-04-16 14:36:48 +0100

    Run pipeline on all test images

--- project/advanced_lane_finding.py
+++ project/advanced_lane_finding.py
@@ -3,6 +3,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
 from p2_functions import calibrate_camera, pipeline
 
 
@@ -10,14 +11,23 @@ if __name__ == "__main__":
   # Get camera calibration info
   ret, mtx, dist, rvecs, tvecs = calibrate_camera('camera_cal/', 9, 6)
   
-  # Get raw image (still distorted)
-  img_raw = mpimg.imread('test_images/test5.jpg')
+  test_images = os.listdir('test_images/') # Get file names for images
 
-  # Undistort the image
-  img = cv2.undistort(img_raw, mtx, dist, None, mtx)
-  
-  # Run undistorted image through pipeline
-  result = pipeline(img, mtx, dist, rvecs, tvecs)
+  for photo in test_images:
+      # Get raw image (still distorted)
+      # img_raw = mpimg.imread('test_images/test6.jpg')
+      img_raw = mpimg.imread('test_images/' + photo)
+
+      # Undistort the image
+      img = cv2.undistort(img_raw, mtx, dist, None, mtx)
+      
+      # Run undistorted image through pipeline
+      result = pipeline(img, mtx, dist, rvecs, tvecs)
+
+      savefile = os.path.splitext(photo)[0]
+      savefile += '_out.jpg'
+
+      cv2.imwrite('./output_images/' + savefile, cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
 
   # Compare side by side
   f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
