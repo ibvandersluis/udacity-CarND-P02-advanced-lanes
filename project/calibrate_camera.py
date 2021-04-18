09cd7a1b3694eaa44ee56d63c1e6666d84cf99b8
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2021-04-18 18:37:29 +0100

    Output sample undistorted photo

--- project/calibrate_camera.py
+++ project/calibrate_camera.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from p2_functions import calibrate_camera
 
 if __name__ == '__main__':
@@ -9,3 +12,12 @@ if __name__ == '__main__':
     print(mtx)
     print('dist')
     print(dist)
+
+    img = mpimg.imread('test_images/test5.jpg')
+    
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    cv2.imwrite('./output_images/undistorted_road.jpg', cv2.cvtColor(undist, cv2.COLOR_RGB2BGR))
+
+    plt.imshow(undist)
+    plt.show()
